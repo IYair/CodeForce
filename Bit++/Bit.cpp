a5e36b6020de1509576e5baecec20bdaa41d19a0
iYair <al046638@uacam.mx>
2022-01-06 11:20:21 -0600

    resolve

---
+++ Bit++/Bit.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n ;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+    }
+    
+
+
+}
---
+++ DominoPiling/Domino.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int m,n;
+    cin >> m >> n;
+
+    int x = m * n;
+    
+    int result = x/2;
+
+    cout << result << endl;
+}
