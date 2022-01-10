fa8e740da4b30a0f91d998fd609531a65a1e9555
EnyaKali <al046638@uacam.mx>
2022-01-09 18:43:34 -0600

    Problema resuelto Bit++

--- Bit++/Bit.cpp
+++ Bit++/Bit.cpp
@@ -2,16 +2,24 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-    int n ;
-    cin >> n;
+int main(int argc, char const *argv[]){
 
-    for (int i = 0; i < n; i++)
-    {
-        /* code */
+	int n;
+    	cin >> n;
+
+    int x = 0;
+    for(int i = 0; i < n; i++ ){
+        string input;
+        cin >> input;
+        if(input[0] == '+')
+            x++;
+        if(input[0] == '-')
+            x--;
+        if(input[2] == '+')
+            x++;
+        if(input[2] == '-')
+            x--;
     }
-    
-
-
+    cout << x << endl;
+    return 0;
 }
